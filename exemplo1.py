7ec3efb792f78d55723d63096916a27cb5e82cb3
ananicole <ana.nicole02@gmail.com>
2023-04-11 16:40:23 -0300

    identify faces in image

--- exemplo1.py
+++ exemplo1.py
@@ -8,4 +8,10 @@ imageCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 facesDetectadas = classificador.detectMultiScale(imageCinza)
 
-print(len(facesDetectadas))
+for (x, y, l, a) in facesDetectadas:
+    cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+
+
+cv2.imshow("Faces encontradas", imagem)
+
+cv2.waitKey()
